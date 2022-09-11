08e3e24bb111573a82d545e78e07baa33fcea632
Numb4r <yuri.d.d.faria@gmail.com>
2022-09-11 12:38:28 -0300

    criado menu em generateGraph

--- generateGraph.py
+++ generateGraph.py
@@ -2,9 +2,15 @@ import matplotlib.pyplot as plt
 import json
 
 dictionary = json.load(open('export.json', 'r'))
+string = ""
+while len(string) == 0:
+    op = int(input("1.SPAM\n2.HAM\n"))
+    if op == 1:
+        string = "SPAM"
+    elif op == 2:
+        string = "HAM"
 
-
-dicToListSorted = sorted(dictionary["HAM"].items(), key=lambda x:x[1])
+dicToListSorted = sorted(dictionary[string].items(), key=lambda x: x[1])
 print(dicToListSorted[-6:-1])
 sortdict = dict(dicToListSorted[-6:-1])
 
@@ -16,15 +22,15 @@ yAxis = [value for key, value in sortdict.items()]
 plt.grid(True)
 
 ## LINE GRAPH ##
-plt.plot(xAxis,yAxis, color='maroon', marker='o')
+plt.plot(xAxis, yAxis, color='maroon', marker='o')
 plt.xlabel('Termos')
 plt.ylabel('Ocorrencias')
 
 ## BAR GRAPH ##
 fig = plt.figure()
-plt.bar(xAxis,yAxis, color='maroon')
+plt.bar(xAxis, yAxis, color='maroon')
 plt.xlabel('variable')
 plt.ylabel('value')
 
 plt.show()
-plt.savefig('HAM.png')
+plt.savefig(string)
